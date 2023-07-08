9da82b512fe5e8b3fc4f1854671cb14c5218ca38
cyleecode <chunyitlee@gmail.com>
2023-07-08 23:04:31 +0800

    Update home page > button -> register

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,13 +1,16 @@
 import { useNavigate } from "react-router-dom";
 import styles from "./Landing.module.scss";
+import { ContextManager } from "../../contexts/all-context";
 const Landing = () => {
+  const _c = ContextManager();
   const navigate = useNavigate();
   const handleLogin = () => navigate("/login");
+  const handleRegister = () => navigate("/register");
   return (
     <>
       <div className={`divCenter ${styles.itemGap}`}>
         <button onClick={handleLogin}>Sign in</button>
-        <button>Sign Up</button>
+        <button onClick={handleRegister}>Sign up</button>
       </div>
     </>
   );
