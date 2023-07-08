3a48f8c443d9ed4560bdb201c1d073ef78d0ac34
cyleecode <chunyitlee@gmail.com>
2023-07-08 23:05:13 +0800

    Update Routes

--- src/App.Routes.tsx
+++ src/App.Routes.tsx
@@ -1,10 +1,24 @@
-import { Route, Routes } from "react-router-dom";
-
-import { Approve, Kyc, Login, Order, Register } from "./components";
+import { Navigate, Route, Routes } from "react-router-dom";
+import {
+  HomePage,
+  LoginPage,
+  ResetPage,
+  LandingPage,
+  RegisterPage,
+  ActivatePage,
+} from "./pages";
 const AppRouting = () => {
   return (
     <>
-      <Routes>{/* <Route path="/" element={<Order />}></Route> */}</Routes>
+      <Routes>
+        <Route path="/login" element={<LoginPage />}></Route>
+        <Route path="/home" element={<HomePage />}></Route>
+        <Route path="/reset" element={<ResetPage />}></Route>
+        <Route path="/register" element={<RegisterPage />}></Route>
+        <Route path="/activate/:email" element={<ActivatePage />}></Route>
+        <Route path="/" element={<LandingPage />}></Route>
+        <Route path="*" element={<Navigate to="/"></Navigate>}></Route>
+      </Routes>
     </>
   );
 };
---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import LoginPage from "./Login/Login";
+import ResetPage from "./RecoverPassword/RecoverPassword";
+import HomePage from "./Home/Home";
+import ActivatePage from "./Activate/Activate";
+import LandingPage from "./Landing/Landing";
+import RegisterPage from "./Register/Register";
+export {
+  LoginPage,
+  HomePage,
+  ResetPage,
+  ActivatePage,
+  LandingPage,
+  RegisterPage,
+};
